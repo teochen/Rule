5fc1c612b371d09a2f399018e7528447f755f866
teochen <youyoubeibei@gmail.com>
2025-08-24 22:42:31 +0800

    Update jinjian.js

--- jinjian.js
+++ jinjian.js
@@ -10,21 +10,31 @@ hostname = closet.jinjian.tech
 // 解析响应数据
 let body = JSON.parse($response.body);
 
-// 修改会员状态
-if (body.data) {
-    // 设置为永久会员
-    body.data.type = "studio.2players.wardrobe.pro.lifetime";
-    body.data.type_text = "永久会员";
-    body.data.expired_at = "2099-09-09";
-    body.data.apple_app_store_auto_renew = true;
-    
-    // 确保有原始购买日期
-    if (!body.data.original_purchase_date_ms) {
-        body.data.original_purchase_date_ms = "1663136602000";
+function modifyObject(obj) {
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            if (typeof obj[key] === 'object' && obj[key] !== null) {
+                // 递归处理嵌套对象
+                modifyObject(obj[key]);
+            } else {
+                // 使用原混淆脚本中的确切值
+                if (key === 'type') {
+                    obj[key] = 'studio.2players.wardrobe.pro.lifetime';
+                }
+                if (key === 'type_text') {
+                    obj[key] = '永久会员';
+                }
+                if (key === 'expired_at') {
+                    obj[key] = '2099-09-09';
+                }
+                // 确保自动续订为true
+                if (key === 'apple_app_store_auto_renew') {
+                    obj[key] = true;
+                }
+            }
+        }
     }
 }
 
-// 返回修改后的数据
-$done({
-    body: JSON.stringify(body)
-});
+modifyObject(body);
+$done({ 'body': JSON.stringify(body) });
